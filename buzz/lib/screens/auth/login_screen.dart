07da25249ca543b79cfed8b211fec3b29b1b9cb9
PedrinHm <pedrohm441@gmail.com>
2024-10-13 14:55:43 -0300

    Redefinição de senha primeiro login

--- buzz/lib/screens/auth/login_screen.dart
+++ buzz/lib/screens/auth/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:buzz/screens/Auth/forgot_password_screen.dart';
+import 'package:buzz/screens/Auth/reset_password_screen.dart'; // Tela de redefinição de senha
 import 'package:buzz/screens/main_screen.dart';
 import 'package:buzz/widgets/Geral/Input_Field.dart';
 import 'package:buzz/widgets/Geral/Button_One.dart';
@@ -41,10 +42,8 @@ class _LoginScreenState extends State<LoginScreen> {
       final String email = emailController.text;
       final String password = passwordController.text;
 
-      print('Sending login request with email: $email and password: $password');
       final response = await http.post(
-        Uri.parse(
-            'https://buzzbackend-production.up.railway.app/auth/'), // Verifique se a URL está correta
+        Uri.parse('https://buzzbackend-production.up.railway.app/auth/'),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
@@ -54,52 +53,53 @@ class _LoginScreenState extends State<LoginScreen> {
         }),
       );
 
-      print('Response received. Status code: ${response.statusCode}');
-      print('Response body: ${response.body}');
-      if (response.statusCode == 200) {
-        final Map<String, dynamic> responseBody = jsonDecode(response.body);
-        final String status = responseBody['status'];
-        final int userTypeId = responseBody['user_type_id'];
-        final int userId =
-            responseBody['id']; // Extraindo o ID do usuário da resposta
+      final Map<String, dynamic> responseBody = jsonDecode(response.body);
+      final String status = responseBody['status'];
+      final int userTypeId = responseBody['user_type_id'];
+      final int userId = responseBody['id'];
 
-        print('Login successful. User type ID: $userTypeId');
-        if (status == 'success') {
-          Usuario usuario;
+      if (status == 'first_login') {
+        // Redireciona para a tela de redefinição de senha
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ResetPasswordScreen(userId: userId),
+          ),
+        );
+      } else if (status == 'success') {
+        Usuario usuario;
 
-          switch (userTypeId) {
-            case 1:
-              usuario = Usuario(tipoUsuario: 'student', id: userId);
-              break;
-            case 2:
-              usuario = Usuario(tipoUsuario: 'driver', id: userId);
-              break;
-            case 3:
-              usuario = Usuario(tipoUsuario: 'admin', id: userId);
-              break;
-            default:
-              usuario = Usuario(tipoUsuario: 'unknown', id: userId);
-          }
-
-          // Obter o token do dispositivo e atualizar no banco de dados
-          String? deviceToken = await _getDeviceToken();
-          if (deviceToken != null) {
-            await _atualizarDeviceToken(userId, deviceToken);
-          }
-
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => MainScreen(usuario: usuario)),
-          );
+        switch (userTypeId) {
+          case 1:
+            usuario = Usuario(tipoUsuario: 'student', id: userId);
+            break;
+          case 2:
+            usuario = Usuario(tipoUsuario: 'driver', id: userId);
+            break;
+          case 3:
+            usuario = Usuario(tipoUsuario: 'admin', id: userId);
+            break;
+          default:
+            usuario = Usuario(tipoUsuario: 'unknown', id: userId);
         }
+
+        String? deviceToken = await _getDeviceToken();
+        if (deviceToken != null) {
+          await _atualizarDeviceToken(userId, deviceToken);
+        }
+
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MainScreen(usuario: usuario),
+          ),
+        );
       } else if (response.statusCode == 403) {
         setState(() {
           _lockoutEndTime = DateTime.now().add(Duration(minutes: 10));
           _errorMessage =
               'Você excedeu o limite de tentativas, tente novamente mais tarde';
-
-          _startTimer(); // Inicia o cronômetro para atualizar a interface
+          _startTimer();
         });
       } else {
         setState(() {
@@ -110,7 +110,6 @@ class _LoginScreenState extends State<LoginScreen> {
       setState(() {
         _errorMessage = 'Erro durante a autenticação';
       });
-      print('Erro durante a autenticação: $e');
     } finally {
       setState(() {
         _isLoading = false;
@@ -119,21 +118,20 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   void _startTimer() {
-    _timer?.cancel(); // Cancela qualquer cronômetro existente
+    _timer?.cancel();
 
     _timer = Timer.periodic(Duration(seconds: 1), (timer) {
       setState(() {});
 
       if (_lockoutEndTime != null && DateTime.now().isAfter(_lockoutEndTime!)) {
-        _timer
-            ?.cancel(); // Cancela o cronômetro quando o tempo de bloqueio terminar
+        _timer?.cancel();
       }
     });
   }
 
   @override
   void dispose() {
-    _timer?.cancel(); // Cancela o cronômetro ao descartar o widget
+    _timer?.cancel();
     emailController.dispose();
     passwordController.dispose();
     super.dispose();
@@ -149,8 +147,7 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: Center(
         child: Container(
-          padding: EdgeInsets.all(
-              getHeightProportion(context, 16.0)), // Proporção em altura
+          padding: EdgeInsets.all(getHeightProportion(context, 16.0)),
           child: _isLoading
               ? CircularProgressIndicator()
               : Column(
@@ -161,22 +158,17 @@ class _LoginScreenState extends State<LoginScreen> {
                       keyboardType: TextInputType.emailAddress,
                       controller: emailController,
                     ),
-                    SizedBox(
-                        height: getHeightProportion(
-                            context, 20)), // Proporção em altura
+                    SizedBox(height: getHeightProportion(context, 20)),
                     CustomInputField(
                       labelText: 'Senha',
                       obscureText: true,
                       controller: passwordController,
                     ),
-                    SizedBox(
-                        height: getHeightProportion(
-                            context, 20)), // Proporção em altura
+                    SizedBox(height: getHeightProportion(context, 20)),
                     if (_errorMessage.isNotEmpty)
                       Padding(
                         padding: EdgeInsets.only(
-                            bottom: getHeightProportion(
-                                context, 10)), // Proporção em altura
+                            bottom: getHeightProportion(context, 10)),
                         child: Text(
                           _errorMessage,
                           style: TextStyle(color: Colors.red),
@@ -185,8 +177,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     if (_lockoutEndTime != null)
                       Padding(
                         padding: EdgeInsets.only(
-                            bottom: getHeightProportion(
-                                context, 10)), // Proporção em altura
+                            bottom: getHeightProportion(context, 10)),
                         child: Text(
                           'Você poderá tentar novamente em ${_formatTimeRemaining()}',
                           style: TextStyle(color: Colors.red),
@@ -197,7 +188,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       onPressed: _isLoading ||
                               (_lockoutEndTime != null &&
                                   DateTime.now().isBefore(_lockoutEndTime!))
-                          ? () {} // função vazia quando desabilitado
+                          ? () {}
                           : _login,
                     ),
                     TextLinkButton(
---
+++ buzz/lib/screens/auth/reset_password_screen.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:buzz/widgets/Geral/Input_Field.dart'; // Importando CustomInputField
+import 'package:buzz/widgets/Geral/Button_One.dart'; // Importando Button_One
+import 'package:buzz/utils/size_config.dart'; // Importando o SizeConfig
+import 'package:buzz/screens/Auth/login_screen.dart'; // Importando a tela de login
+
+class ResetPasswordScreen extends StatefulWidget {
+  final int userId;
+  ResetPasswordScreen({required this.userId});
+
+  @override
+  _ResetPasswordScreenState createState() => _ResetPasswordScreenState();
+}
+
+class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
+  final TextEditingController newPasswordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+  bool _isLoading = false;
+  String _errorMessage = '';
+
+  Future<void> _setNewPassword() async {
+    if (newPasswordController.text.isEmpty ||
+        confirmPasswordController.text.isEmpty) {
+      setState(() {
+        _errorMessage = 'Por favor, preencha todos os campos';
+      });
+      return;
+    }
+
+    if (newPasswordController.text != confirmPasswordController.text) {
+      setState(() {
+        _errorMessage = 'As senhas não coincidem';
+      });
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+      _errorMessage = '';
+    });
+
+    try {
+      final response = await http.post(
+        Uri.parse(
+            'https://buzzbackend-production.up.railway.app/auth/set-new-password'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, dynamic>{
+          'user_id': widget.userId,
+          'new_password': newPasswordController.text,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        // Exibir o Snackbar verde após a redefinição da senha
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Senha redefinida com sucesso!'),
+            backgroundColor: Colors.green,
+          ),
+        );
+        // Aguarda o tempo do Snackbar e redireciona para a tela de login
+        await Future.delayed(Duration(seconds: 2));
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LoginScreen()),
+        );
+      } else {
+        // Exibir o Snackbar vermelho em caso de erro
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Erro ao redefinir a senha'),
+            backgroundColor: Colors.red,
+          ),
+        );
+        setState(() {
+          _errorMessage = 'Erro ao redefinir a senha';
+        });
+      }
+    } catch (e) {
+      // Exibir o Snackbar vermelho em caso de exceção
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Erro durante a redefinição de senha'),
+          backgroundColor: Colors.red,
+        ),
+      );
+      setState(() {
+        _errorMessage = 'Erro durante a redefinição de senha';
+      });
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(getHeightProportion(context, 16.0)),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CustomInputField(
+                labelText: 'Nova Senha',
+                obscureText: true,
+                controller: newPasswordController,
+              ),
+              SizedBox(height: getHeightProportion(context, 20)),
+              CustomInputField(
+                labelText: 'Confirmar Nova Senha',
+                obscureText: true,
+                controller: confirmPasswordController,
+              ),
+              if (_errorMessage.isNotEmpty)
+                Padding(
+                  padding:
+                      EdgeInsets.only(top: getHeightProportion(context, 10)),
+                  child: Text(
+                    _errorMessage,
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+              SizedBox(height: getHeightProportion(context, 20)),
+              _isLoading
+                  ? CircularProgressIndicator()
+                  : Button_One(
+                      buttonText: 'Definir Senha',
+                      onPressed: () async {
+                        await _setNewPassword(); // Função assíncrona chamada dentro de uma função anônima
+                      },
+                    ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
